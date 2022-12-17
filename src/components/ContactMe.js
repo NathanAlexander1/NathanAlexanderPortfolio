fd2912d312dab741ceea37bcafa0cc3d0bec74bd
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-12-17 14:45:11 -0800

    portfolio draft

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, { useState } from "react";
+import { validateEmail } from "../utils/helpers";
 
-function ContactMe() {
+function ContactInfo() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [reasonForMessage, setReasonForMessage] = useState("employment");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (name === "") {
+      setErrorMessage("Please enter your name");
+      return;
+    } else if (!validateEmail(email)) {
+      setErrorMessage(
+        "Email not valid, please enter a valid email (xxxxxx@xxxxx.xxx)"
+      );
+      return;
+    } else if (message === "") {
+      setErrorMessage(`I'd love to hear from you! Please enter a message!`);
+      return;
+    } else {
+      setErrorMessage("");
+    }
+    setName("");
+    setEmail("");
+    setReasonForMessage("employment");
+    setMessage("");
+    alert(`
+    Name: ${name}
+    Email: ${email}
+    Reason for message: ${reasonForMessage}
+    Message: ${message}`);
+  };
+
   return (
-    <div>ContactMe</div>
-  )
+    <>
+      <h1>Contact me</h1>
+      <div className="contact-page vh-100 d-flex flex-column justify-content-center">
+        <form className="contactForm d-flex flex-column align-self-center">
+          <input 
+            className="form-field"
+            name="name"
+            placeholder="Please enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <input 
+            className="p-2  form-field"
+            name="email"
+            placeholder="Please enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <select 
+            className="p-2  form-field"
+            value={reasonForMessage}
+            onChange={(e) => setReasonForMessage(e.target.value)}
+          >
+            {/* refers to reasonForMessage */}
+            <option value="employment">
+              I am reaching out about an employment opportunity
+            </option>
+            <option value="question">
+              I am reaching out with a specific question
+            </option>
+            <option value="networking">
+              I am reaching out for networking purposes
+            </option>
+            <option value="other">Other</option>
+          </select>
+          <textarea 
+            className="p-2  form-field"
+            name="message"
+            placeholder="Please enter your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button 
+            className="p-2  form-field"
+            type="button"
+            onClick={handleFormSubmit}
+          >
+            Send
+          </button>
+        </form>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
-export default ContactMe
+export default ContactInfo;
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import NathanResume from "../assets/Nathan-Resume.pdf";
+import HTML from "../assets/proficiencies/html.png";
+import CSS from "../assets/proficiencies/css.png";
+import JSLogo from "../assets/proficiencies/jslogo.png";
+import reactLogo from "../assets/proficiencies/reactlogo.png";
+import node from "../assets/proficiencies/nodelogo.webp";
 
 function Resume() {
   return (
-    <div>Resume</div>
-  )
+    <div className="vh-100 d-flex flex-column justify-content-between">
+      <h1 >Download my resume below</h1>
+      <div className="align-self-center">
+        <a href={NathanResume} download>
+          Download my resume
+        </a>
+      </div>
+      <div className="d-flex flex-column vw-100">
+        <p className="align-self-center">Proficiencies</p>
+        <div className="proficiencies ">
+          <div className="logosContainer d-flex flex-wrap justify-content-around">
+            <div className="individualLogos">
+              <img src={HTML} alt="html logo" height="80" />
+              <p>HTML</p>
+            </div>
+            <div className="individualLogos">
+              <img src={CSS} alt="css logo" height="80" />
+              <p>CSS</p>
+            </div>
+            <div className="individualLogos">
+              <img src={JSLogo} alt="javascript logo" height="80" />
+              <p>JavaScript</p>
+            </div>
+            <div className="individualLogos">
+              <img src={reactLogo} alt="react logo" height="80" />
+              <p>React.js</p>
+            </div>
+            <div className="individualLogos">
+              <img src={node} alt="node logo" height="80" />
+              <p>Node.js</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="resumeDiv pageNames d-flex flex-column align-items-center">
+        <p>I am also proficient in ....................</p>
+      </div>
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
